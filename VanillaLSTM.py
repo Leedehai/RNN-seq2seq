126cba710067f730ab56ef668b709d0fdde3a5a5
Leedehai <hhlisu@yahoo.com>
2017-03-05 17:17:38 -0800

    VanillaLSTM.py test passed. The problem of mistached decoder cell input shape is solved.

--- VanillaLSTM.py
+++ VanillaLSTM.py
@@ -1,6 +1,8 @@
 """
-Vanilla LSTM model implemented with TensorFlow
-CS224N Project
+Vanilla LSTM model implemented with TensorFlow 0.12.1, Python 2.7.12
+- - - - -
+CS224N Project at Stanford Univeristy
+Project mentor: Prof. Chirs Manning
 
 Author: Haihong Li
 Date: March 1, 2017
@@ -14,7 +16,6 @@ import argparse
 import pdb
 
 import rnn_segment
-import temp_optimizer
 
 # The vanilla LSTM model
 class VanillaLSTMModel():
@@ -30,21 +31,23 @@ class VanillaLSTMModel():
 		    args.hidden_size (i.e. the size of hidden state)
 		    args.num_layers (default = 1, i.e. no stacking), 
 		    args.seq_length, 
-		    args.embedding_size, 
+		    args.input_embedding_size
+			args.output_embedding_size
 		    args.batch_size (i.e. the number of sequences in each batch)
 		    args.optimizer_choice (defualt = "rms", also could be "adam", "grad_desc")
 		    args.learning_rate, 
 		    args.grad_clip
-		NOTE Each cell's input is batch_size x 1 x embedding_size
-		NOTE Each cell's output is also batch_size x 1 x embedding_size
+		NOTE Each cell's input is batch_size x 1 x input_embedding_size
+		NOTE Each cell's output is batch_size x 1 x output_embedding_size
 		'''
 		# Store the arguments, and print the important argument values
 		self.args = args
 		print("VanillaLSTMModel initializer is called..\n" \
 		      + "Time: " + time.ctime() + "\n" \
-			  + "  args.hidden_size = " + str(args.hidden_size) + "\n" \
+			  + "  args.hidden_size (H) = " + str(args.hidden_size) + "\n" \
+			  + "  args.input_embedding_size (Di) = " + str(args.input_embedding_size) + "\n" \
+			  + "  args.output_embedding_size (Do) = " + str(args.output_embedding_size) + "\n" \
 			  + "  args.num_layers = " + str(args.num_layers) + "\n" \
-			  + "  args.embedding_size = " + str(args.embedding_size) + "\n" \
 			  + "  args.optimizer_choice = " + args.optimizer_choice + "\n" \
 			  + "  args.learning_rate = " + str(args.learning_rate) + "\n" \
 			  + "  args.grad_clip = " + str(args.grad_clip) + "\n")
@@ -58,8 +61,13 @@ class VanillaLSTMModel():
 
 		# initialize a LSTM cell unit, hidden_size is the dimension of hidden state
 		# TODO: (resolve) are the two kinds of cell's hidden state of the same size?
-		encoder_cell = tf.nn.rnn_cell.BasicLSTMCell(args.hidden_size, state_is_tuple=True)
-		decoder_cell = tf.nn.rnn_cell.BasicLSTMCell(args.hidden_size, state_is_tuple=True)
+		with tf.variable_scope("vanLSTM_encoder"):
+			encoder_cell = tf.nn.rnn_cell.BasicLSTMCell(args.hidden_size, state_is_tuple=True)
+		with tf.variable_scope("vanLSTM_decoder"):
+			decoder_cell = tf.nn.rnn_cell.BasicLSTMCell(args.hidden_size, state_is_tuple=True)
+			# y_hat = softmax(tf.add(tf.matmul(cell_output, output_ws), output_bs)), output_bs = zeros, for now
+			self.output_ws = tf.get_variable("output_ws", [args.hidden_size, args.output_embedding_size])
+			self.output_converter_lambda = lambda cell_output: tf.nn.softmax(logits=tf.matmul(cell_output, self.output_ws), dim=1)
 
 		# TODO: (improve) Multi-layer RNN ocnstruction, if more than one layer
 		# encoder_cell = rnn_cell.MultiRNNCell([encoder_cell] * args.num_layers, state_is_tuple=True)
@@ -71,10 +79,10 @@ class VanillaLSTMModel():
 		self.decoder_cell = decoder_cell
 
 		# TODO: (resolve) do we need to use a fixed seq_length?
-		# Input data contains sequences of input tokens of embedding_size dimension
-		self.input_data = tf.placeholder(tf.float32, [None, args.seq_length, args.embedding_size])
-		# Target data contains sequences of output tokens of embedding_size dimension
-		self.target_data = tf.placeholder(tf.float32, [None, args.seq_length, args.embedding_size])
+		# Input data contains sequences of input tokens of input_embedding_size dimension
+		self.input_data = tf.placeholder(tf.float32, [None, args.seq_length, args.input_embedding_size])
+		# Target data contains sequences of output tokens of output_embedding_size dimension
+		self.target_data = tf.placeholder(tf.float32, [None, args.seq_length, args.output_embedding_size])
 
         # Learning rate
 		self.lr = tf.Variable(args.learning_rate, trainable=False, name="learning_rate")
@@ -82,41 +90,48 @@ class VanillaLSTMModel():
 		# Initial cell state of LSTM (initialized with zeros)
 		# TODO: (improve) might use xavier initializer? There seems to be no a staright-forward way
 		self.initial_state = encoder_cell.zero_state(batch_size=args.batch_size, dtype=tf.float32)
-		self.initial_state = decoder_cell.zero_state(batch_size=args.batch_size, dtype=tf.float32)
 
-		# Split inputs according to sequences: a 3D tensor, num_of_seq x seq_length x embedding_size
-		# -> list of size seq_length, each of whose element is of num_of_seq x 1 x embedding_size
+		# Split inputs according to sequences: a 3D Tensor, num_of_seq x seq_length x input_embedding_size
+		# -> list of size seq_length, each of whose element is of num_of_seq x 1 x input_embedding_size
 		if tf.__version__[0:2] == '0.':
 			input_data_temp = tf.split(split_dim=1, num_split=args.seq_length, value=self.input_data)
 		elif tf.__version__[0:2] == '1.':
 			input_data_temp = tf.split(value=self.input_data, num_split=args.seq_length, split_dim=1)
-		# list of size seq_length, each of which is num_of_seq x 1 x input_token_size
-		# -> list of size seq_length, each of which is num_of_seq x input_token_size
+		# list of size seq_length, each of which is num_of_seq x 1 x input_embedding_size
+		# -> list of size seq_length, each of which is num_of_seq x input_embedding_size
 		input_data_list = [tf.squeeze(input=inputs_member, axis=[1]) for inputs_member in input_data_temp]
 		del input_data_temp
 		
 		## This is where the LSTM models differ from each other in substance.
 		## The other code might also differ but they are not substantial.
 		# call the encoder
-		_, self.encoder_final_state = rnn_segment.run(cell=self.encoder_cell, 
-		                                         inputs=input_data_list, 
-												 initial_state=self.initial_state, 
-												 feed_previous=False,
-												 scope="vanLSTM_encoder")
+		print("[DEBUG] self.initial_state: " + str(self.initial_state))
+		#with tf.variable_scope("vanLSTM_encoder"):
+		_, self.encoder_final_state = rnn_segment.run(cell=encoder_cell, 
+													      inputs=input_data_list, 
+													      initial_state=self.initial_state, 
+													      feed_previous=False,
+														  loop_func=None,
+													      scope="vanLSTM_encoder")
 		# call the decoder
+
+		#with tf.variable_scope("vanLSTM_decoder"):
+		print("[DEBUG] self.encoder_final_state: " + str(self.encoder_final_state))
+		print("[DEBUG] self.decoder_inital_state: " + str(self.decoder_cell.zero_state(batch_size=args.batch_size, dtype=tf.float32)))
 		self.output_data = self.input_data
-		#self.output_data, _ = rnn_segment.run(cell=self.decoder_cell, 
-		#                                      inputs=input_data_list, 
-		#									  initial_state=self.encoder_final_state, 
-		#									  feed_previous=True,
-		#									  scope="vanLSTM_decoder")
+		self.output_data, _ = rnn_segment.run(cell=decoder_cell, 
+												  inputs=input_data_list, 
+												  initial_state=self.encoder_final_state, 
+												  feed_previous=True,
+												  loop_func=self.output_converter_lambda,
+												  scope="vanLSTM_decoder")
 		
 		
 		def get_sum_of_cost(output_data, target_data):
 			'''
 			Calculate the sum of cost of this training batch using cross entropy.
 			params:
-			output_data: batch_size x seq_length x embedding_size
+			output_data: batch_size x seq_length x output_embedding_size
 			target_data: batch_size x seq_length x output_vocab_size (one-hot)
 			'''
 			# TODO: (unfinished) finsh this function
@@ -127,25 +142,32 @@ class VanillaLSTMModel():
 		sum_of_cost = get_sum_of_cost(output_data=self.output_data, target_data=self.target_data)
 		#self.cost = tf.Variable(0.)
 		self.cost = tf.div(sum_of_cost, args.batch_size)
-		print("\nself.cost:")
-		print self.cost
+		print("\n[DEBUG] self.cost: ") + str(self.cost)
+
 		# Get trainable_variables list, print them, and compute the gradients
 		# Also clip the gradients if they are larger than args.grad_clip
 		trainable_vars = tf.trainable_variables()
-		print("\nNumber of trainable variables = " + str(len(trainable_vars)))
+		num_trainable_components = 0
+		print("\nNumber of trainable Tensors = " + str(len(trainable_vars)))
 		for i, var in enumerate(trainable_vars):
-			print(str(trainable_vars[i].name) + \
-			      "\t" + str(trainable_vars[i].get_shape()) + \
+			num_trainable_components += np.product(trainable_vars[i].get_shape().as_list())
+			print(" " + str(trainable_vars[i].name) + \
+			      "\t"  + str(trainable_vars[i].get_shape()) + \
 				  " x " + str(trainable_vars[i].dtype.name))
+		print("Number of trainable scalar components = " + str(num_trainable_components) + "\n")
+		self.num_of_trainable_components = num_trainable_components
+
 		# self.gradients is a list of tuples of (grad_value, variable_name)
 		self.gradients = tf.gradients(self.cost, trainable_vars)
 		if args.model_unit_test_flag == True:
 			for i in xrange(len(self.gradients)):
 				if self.gradients[i] == None:
 					self.gradients[i] = tf.zeros(shape=trainable_vars[i].get_shape(), dtype=tf.float32)
-		print("\nself.gradients: length = " + str(len(self.gradients)))
-		print self.gradients
-		clipped_grads, _ = tf.clip_by_global_norm(self.gradients, args.grad_clip)
+
+		if args.grad_clip != None:
+			clipped_grads, _ = tf.clip_by_global_norm(self.gradients, args.grad_clip)
+		else:
+			clipped_grads = self.gradients
 
 		# Using RMSprop, inspired by the LSTM paper of Dr. Alahi, Prof. Saverese, and Prof. Fei-Fei Li
 		if args.optimizer_choice == "rms":
@@ -157,13 +179,8 @@ class VanillaLSTMModel():
 		else:
 			raise ValueError("Optimizer not supported: " + args.optimizer_choice)
 
-		# Train operator
-		temp_zipped = zip(clipped_grads, trainable_vars)
-		print("\ntemp_zipped: length = " + str(len(temp_zipped)))
-		print temp_zipped
-        #pdb.set_trace()
-		# Apply gradients. If a gradient of a variable is None, it will be weeded out.
-		self.train_op = optimizer.apply_gradients(temp_zipped)
+		# Train operator. Apply gradients. If a gradient of a variable is None, it will be weeded out.
+		self.train_op = optimizer.apply_gradients(zip(clipped_grads, trainable_vars))
 
 def main():
 	'''
@@ -171,7 +188,8 @@ def main():
 	    args.hidden_size (i.e. the size of hidden state)
 		args.num_layers (default = 1, i.e. no stacking), 
 		args.seq_length, 
-	    args.embedding_size, 
+	    args.input_embedding_size, 
+		args.output_embedding_size,
 		args.batch_size (i.e. the number of sequences in each batch)
 		args.optimizer_choice (defualt = "rms", also could be "adam", "grad_desc")
 		args.learning_rate, 
@@ -179,19 +197,22 @@ def main():
 	'''
 	parser = argparse.ArgumentParser()
 	# RNN cell hidden state's size
-	parser.add_argument('--hidden_size', type=int, default=1,
+	parser.add_argument('--hidden_size', type=int, default=3,
 	                    help='size of RNN cell hidden state')
 	# Number of stacked RNN layers. Only a single layer implemented
 	parser.add_argument('--num_layers', type=int, default=1,
 	                    help='number of stacked RNN layers')
 	# Maximum length of each sequence
-	parser.add_argument('--seq_length', type=int, default=1,
+	parser.add_argument('--seq_length', type=int, default=12,
 	                    help='maximum length of each sequence')
-	# Embedding size
-	parser.add_argument('--embedding_size', type=int, default=1,
-	                    help='embedding size of vectors')
+	# Embedding size of input
+	parser.add_argument('--input_embedding_size', type=int, default=2,
+	                    help='embedding size of input vectors')
+	# Embedding size of output
+	parser.add_argument('--output_embedding_size', type=int, default=2,
+	                    help='embedding size of output vectors')
 	# Batch size
-	parser.add_argument('--batch_size', type=int, default=1,
+	parser.add_argument('--batch_size', type=int, default=100,
 	                    help='number of sequences in a batch')
 	# Choice of optimzier
 	parser.add_argument('--optimizer_choice', type=str, default='rms',
@@ -200,7 +221,7 @@ def main():
 	parser.add_argument('--learning_rate', type=float, default=0.002,
 	                    help='Learning rate')	
 	# Gradient clip, i.e. maximum value of gradient amplitute allowed
-	parser.add_argument('--grad_clip', type=float, default=10.0,
+	parser.add_argument('--grad_clip', type=float, default=None,
 	                    help='gradient upbound, i.e. maximum value of gradient amplitute allowed')
 	# Model unit testing flag
 	parser.add_argument('--model_unit_test_flag', type=bool, default=True,
@@ -214,3 +235,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
+	print("this module is functioning.") # test passed. March 5, 2017
--- rnn_segment.py
+++ rnn_segment.py
@@ -1,9 +1,11 @@
 """
 RNN segment functions
-CS224N Project
+- - - - -
+CS224N Project at Stanford Univeristy
+Project mentor: Prof. Chirs Manning
 
 Author: Haihong Li
-Date: March 3, 2017
+Date: March 1, 2017
 
 RNN segment: a train of RNN cells (stacked vertically or single layer) linked together in series.
                o u t p u t s
@@ -15,46 +17,57 @@ initial_state -o-o-o-...-o- final_state
 
 import tensorflow as tf
 import numpy as np
-import ipdb
-from tensorflow.python.util import nest
 
-def run(cell, inputs, initial_state, feed_previous=False, scope="rnn"):
+import pdb
+
+def run(cell, inputs, initial_state, feed_previous=False, loop_func=None, scope="segment_default"):
 	'''
 	RNN segment works.
 	Params:
 	    cell: RNN cell, created by tf.nn.rnn_cell.BasicLSTMCell or else.
 	    inputs: list of Tensors, variable length, each element is of size 
-	        batch_size x embedding_size
+	        batch_size x (input/output)_embedding_size
 	    initial_state: the initial state, of size batch_size x hidden_size
 	    feed_previous: if True, then a cell's input is the previous cell's
-	        output, with the exception of the first cell, whose input is
-		    the first element of the input variable: inputs.
+	        output processed by loop_func, with the exception of the first cell, whose input is
+		    the first element of the input variable: inputs
+		loop_func: explained above 
 		scope: VariableScope for the created subgraph
 	 Returns:
 	     outputs: list of tensors, length equals to the inputs, each
 		     element is of size batch_size x cell_output_size
 		 state: the hidden state in the end of this cell segment
 	'''
-
+	if feed_previous == True and loop_func == None:
+		pass
+		#raise ValueError("feed_previous is True, but loop_func is not given")
 	state = initial_state
 	outputs = []
-
+	
 	#outputs, state = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state)
 	#return (outputs, state)
-	print "run() called"
+	print "\n\033[32m[DEBUG] an rnn_segement.run() is linked into the computational graph in scope " + scope + "\033[m"
 	with tf.variable_scope(scope):
 		cell_state = initial_state
+		print("\n[DEBUG] initial_state: " + str(initial_state))
 		outputs = []
 		prev_cell_output = None
+		#pdb.set_trace()
 		for i, cell_input in enumerate(inputs):
 			if (feed_previous == True) and (prev_cell_output != None):
-				cell_input = prev_cell_output
+				cell_input = prev_cell_output_converted
 			if i > 0:
 				tf.get_variable_scope().reuse_variables()
-			cell_output, cell_state = cell(cell_input, cell_state)
+				#pdb.set_trace()
+			print("\n[DEBUG] \033[34mBEFORE CELL " + scope + "\n" + str(i) + " \033[mcell_input: " + str(cell_input))
+			print("\n[DEBUG] \033[34mBEFORE CELL " + scope + "\n" + str(i) + " \033[mcell_state: " + str(cell_state))
+			cell_output, cell_state = cell(cell_input, cell_state, scope)
+			print("\n[DEBUG] \033[36mAFTER CELL " + scope + "\n" + str(i) + " \033[mcell_output: " + str(cell_output))
+			print("\n[DEBUG] \033[36mAFTER CELL " + scope + "\n" + str(i) + " \033[mcell_state: " + str(cell_state))
 			outputs.append(cell_output)
 			if feed_previous == True:
-				prev_cell_output = cell_output
+				prev_cell_output_converted = loop_func(cell_output)
+				#print("\n[DEBUG] \033[33mPREV_CELL_OUTPUT " + scope + "\n" + str(i) + " \033[mprev_cell_output: " + str(prev_cell_output))
 	return outputs, cell_state
 
 
